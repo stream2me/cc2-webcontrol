f748efb94a5362e695d06d8743cdc5ab67aea946
stream2me <32234535+stream2me@users.noreply.github.com>
2026-07-01 08:40:42 +0000

    add windows build cmd and small fix

--- build.rs
+++ build.rs
@@ -1,19 +1,27 @@
 use std::process::Command;
 
 fn main() {
+    if std::env::var("SKIP_FRONTEND_BUILD").is_ok() {
+        println!("cargo:warning=Frontend-Build skipped.");
+        return;
+    }
+
     println!("cargo:rerun-if-changed=frontend/src");
     println!("cargo:rerun-if-changed=frontend/index.html");
     println!("cargo:rerun-if-changed=frontend/package.json");
     println!("cargo:rerun-if-changed=frontend/vite.config.ts");
 
-    let status = Command::new("sh")
-        .arg("-c")
-        .arg("cd frontend && npm install && npm run build")
-        .status();
+    let mut cmd = if cfg!(target_os = "windows") {
+        let mut c = Command::new("cmd");
+        c.args(&["/C", "cd frontend && npm install && npm run build"]);
+        c
+    } else {
+        let mut c = Command::new("sh");
+        c.args(&["-c", "cd frontend && npm install && npm run build"]);
+        c
+    };
 
-    if std::env::var("SKIP_FRONTEND_BUILD").is_ok() {
-        return;
-    }
+    let status = cmd.status();
 
     match status {
         Ok(s) if s.success() => {}
