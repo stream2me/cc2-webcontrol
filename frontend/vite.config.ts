1faa8933cd8a3fd134f00ecf1b0e4831e07b79c4
stream2me <32234535+stream2me@users.noreply.github.com>
2026-07-01 08:56:29 +0000

    add gz support, small optimization Support for GZ compression to reduce the load on the CPU Optimization of the local build process

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,36 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 1024,
+      deleteOriginFile: false
+    })
+  ],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    chunkSizeWarningLimit: 3000,
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('plotly')) {
+              return 'vendor-plotly';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
+  esbuild: {
+    drop: ['console', 'debugger'],
+  }
 });
